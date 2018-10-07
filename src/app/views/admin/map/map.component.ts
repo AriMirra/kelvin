226984636533243a62be72f0e1ccb0cf0f93b2a5
Tomas Perez Molina <tomas.perezmolina@ing.austral.edu.ar>
2018-10-07 20:02:39 -0300

    Merge map component

--- src/app/views/admin/map/map.component.ts
+++ src/app/views/admin/map/map.component.ts
@@ -11,8 +11,8 @@ import * as Rx from 'rxjs/internal/operators';
 import {forkJoin, Observable} from 'rxjs';
 import {Report} from '../../../../shared/reports/Report';
 import {Time} from '@angular/common';
-import {ReportService} from '../../../services/report.service';
 import {ReportParameters} from '../../../../shared/reports/ReportParameters';
+import {ReportService} from '../../../services/report.service';
 
 @Component({
   templateUrl: 'map.component.html',
@@ -45,6 +45,138 @@ export class AdminMapComponent implements OnInit {
     popupAnchor: [0, 0] // point from which the popup should open relative to the iconAnchor
   });
 
+  // main chart
+
+  public mainChartElements = 27; // TODO Report size
+
+  // temperature
+  public maxTemperature: Array<number> = [];
+  public currentTemperature: Array<number> = [];
+  public minTemperature: Array<number> = [];
+
+  // moisture
+  public maxMoisture: Array<number> = [];
+  public currentMoisture: Array<number> = [];
+  public minMoisture: Array<number> = [];
+
+  public mainChartLabels: Array<string> = [];
+  public mainChartData: Array<any> = [
+      {
+          data: this.maxTemperature,
+          label: 'Temperatura Máxima'
+      },
+      {
+          data: this.currentTemperature,
+          label: 'Temperatura'
+      },
+      {
+          data: this.minTemperature,
+          label: 'Temperatura Mínima'
+      },
+      {
+          data: this.maxMoisture,
+          label: 'Humedad Máxima'
+      },
+      {
+          data: this.currentMoisture,
+          label: 'Humedad'
+      },
+      {
+          data: this.minMoisture,
+          label: 'Humedad Mínima'
+      },
+
+  ];
+  public mainChartOptions: any = {
+      tooltips: {
+          enabled: false,
+          custom: CustomTooltips,
+          intersect: true,
+          mode: 'index',
+          position: 'nearest',
+          callbacks: {
+              labelColor: function(tooltipItem, chart) {
+                  return { backgroundColor: chart.data.datasets[tooltipItem.datasetIndex].borderColor };
+              }
+          }
+      },
+      responsive: true,
+      maintainAspectRatio: false,
+      scales: {
+          xAxes: [{
+              gridLines: {
+                  drawOnChartArea: false,
+              },
+          }],
+          yAxes: [{
+              ticks: {
+                  beginAtZero: true,
+                  maxTicksLimit: 5,
+                  stepSize: Math.ceil(100 / 10),
+                  max: 100
+              }
+          }]
+      },
+      elements: {
+          line: {
+              borderWidth: 2
+          },
+          point: {
+              radius: 0,
+              hitRadius: 10,
+              hoverRadius: 4,
+              hoverBorderWidth: 3,
+          }
+      },
+      legend: {
+          display: false
+      }
+  };
+  public mainChartColours: Array<any> = [
+      { // max-temperature
+          backgroundColor: 'transparent',
+          borderColor: getStyle('--danger'),
+          pointHoverBackgroundColor: '#fff',
+      },
+      { // temperature
+          backgroundColor: hexToRgba(getStyle('--success'), 10),
+          borderColor: getStyle('--success'),
+          pointHoverBackgroundColor: '#fff'
+      },
+      { // min-temperature
+          backgroundColor: 'transparent',
+          borderColor: getStyle('--info'),
+          pointHoverBackgroundColor: '#fff',
+      },
+      { // max-moisture
+          backgroundColor: 'transparent',
+          borderColor: getStyle('--danger'),
+          pointHoverBackgroundColor: '#fff',
+          borderWidth: 1,
+          borderDash: [8, 5]
+      },
+      { // moisture
+          backgroundColor: 'transparent',
+          borderColor: getStyle('--success'),
+          pointHoverBackgroundColor: '#fff',
+          borderWidth: 1,
+          borderDash: [8, 5]
+      },
+      { // min-moisture
+          backgroundColor: 'transparent',
+          borderColor: getStyle('--info'),
+          pointHoverBackgroundColor: '#fff',
+          borderWidth: 1,
+          borderDash: [8, 5]
+      },
+  ];
+  public mainChartLegend = false;
+  public mainChartType = 'line';
+
+  public random(min: number, max: number) { // TODO delete
+      return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
   constructor(private userService: UserService, private vehicleService: VehicleService, private reportService: ReportService) {
     this.userService.fetchUsers().subscribe(users => {
       this.users = users;
@@ -57,153 +189,22 @@ export class AdminMapComponent implements OnInit {
       });
     });
   }
-    // main chart
 
-    public mainChartElements = 27; // TODO Report size
-
-    // temperature
-    public maxTemperature: Array<number> = [];
-    public currentTemperature: Array<number> = [];
-    public minTemperature: Array<number> = [];
-
-    // moisture
-    public maxMoisture: Array<number> = [];
-    public currentMoisture: Array<number> = [];
-    public minMoisture: Array<number> = [];
-
-    public mainChartLabels: Array<string> = [];
-    public mainChartData: Array<any> = [
-        {
-            data: this.maxTemperature,
-            label: 'Temperatura Máxima'
-        },
-        {
-            data: this.currentTemperature,
-            label: 'Temperatura'
-        },
-        {
-            data: this.minTemperature,
-            label: 'Temperatura Mínima'
-        },
-        {
-            data: this.maxMoisture,
-            label: 'Humedad Máxima'
-        },
-        {
-            data: this.currentMoisture,
-            label: 'Humedad'
-        },
-        {
-            data: this.minMoisture,
-            label: 'Humedad Mínima'
-        },
-
-    ];
-    public mainChartOptions: any = {
-        tooltips: {
-            enabled: false,
-            custom: CustomTooltips,
-            intersect: true,
-            mode: 'index',
-            position: 'nearest',
-            callbacks: {
-                labelColor: function(tooltipItem, chart) {
-                    return { backgroundColor: chart.data.datasets[tooltipItem.datasetIndex].borderColor };
-                }
-            }
-        },
-        responsive: true,
-        maintainAspectRatio: false,
-        scales: {
-            xAxes: [{
-                gridLines: {
-                    drawOnChartArea: false,
-                },
-            }],
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true,
-                    maxTicksLimit: 5,
-                    stepSize: Math.ceil(100 / 10),
-                    max: 100
-                }
-            }]
-        },
-        elements: {
-            line: {
-                borderWidth: 2
-            },
-            point: {
-                radius: 0,
-                hitRadius: 10,
-                hoverRadius: 4,
-                hoverBorderWidth: 3,
-            }
-        },
-        legend: {
-            display: false
-        }
-    };
-    public mainChartColours: Array<any> = [
-        { // max-temperature
-            backgroundColor: 'transparent',
-            borderColor: getStyle('--danger'),
-            pointHoverBackgroundColor: '#fff',
-        },
-        { // temperature
-            backgroundColor: hexToRgba(getStyle('--success'), 10),
-            borderColor: getStyle('--success'),
-            pointHoverBackgroundColor: '#fff'
-        },
-        { // min-temperature
-            backgroundColor: 'transparent',
-            borderColor: getStyle('--info'),
-            pointHoverBackgroundColor: '#fff',
-        },
-        { // max-moisture
-            backgroundColor: 'transparent',
-            borderColor: getStyle('--danger'),
-            pointHoverBackgroundColor: '#fff',
-            borderWidth: 1,
-            borderDash: [8, 5]
-        },
-        { // moisture
-            backgroundColor: 'transparent',
-            borderColor: getStyle('--success'),
-            pointHoverBackgroundColor: '#fff',
-            borderWidth: 1,
-            borderDash: [8, 5]
-        },
-        { // min-moisture
-            backgroundColor: 'transparent',
-            borderColor: getStyle('--info'),
-            pointHoverBackgroundColor: '#fff',
-            borderWidth: 1,
-            borderDash: [8, 5]
-        },
-    ];
-    public mainChartLegend = false;
-    public mainChartType = 'line';
-
-    public random(min: number, max: number) { // TODO delete
-        return Math.floor(Math.random() * (max - min + 1) + min);
+  ngOnInit(): void {
+    // generate random values for mainChart
+    for (let i = 0; i <= this.mainChartElements; i++) { // TODO push data from back
+        this.mainChartLabels.push('' + i);
+        // temperature
+        this.maxTemperature.push(50);
+        this.currentTemperature.push(this.random(0, 50));
+        this.minTemperature.push(0);
+        // moisture
+        this.maxMoisture.push(100);
+        this.currentMoisture.push(this.random(75, 100));
+        this.minMoisture.push(75);
     }
-
-    ngOnInit(): void {
-        // generate random values for mainChart
-        for (let i = 0; i <= this.mainChartElements; i++) { // TODO push data from back
-            this.mainChartLabels.push('' + i);
-            // temperature
-            this.maxTemperature.push(50);
-            this.currentTemperature.push(this.random(0, 50));
-            this.minTemperature.push(0);
-            // moisture
-            this.maxMoisture.push(100);
-            this.currentMoisture.push(this.random(75, 100));
-            this.minMoisture.push(75);
-        }
-        this.newRoute = Route.empty();
-        this.map = L.map('map').setView([-34.61315, -58.37723], 10);
+    this.newRoute = Route.empty();
+    this.map = L.map('map').setView([-34.61315, -58.37723], 10);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
